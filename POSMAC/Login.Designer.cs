d1f0ff0eb763e3ce3b1b3d9e3fe44245667849e6
20laklira <20laklira@gmail.com>
2017-08-25 13:46:09 +0530

    zczxczxczx

--- POSMAC/Login.Designer.cs
+++ POSMAC/Login.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(211, 50);
+            this.button1.Location = new System.Drawing.Point(180, 104);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
