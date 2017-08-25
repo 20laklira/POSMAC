3747b2b2ae40b942e0e2149052d3a392d276a1af
20laklira <20laklira@gmail.com>
2017-08-25 15:25:52 +0530

    lo

--- POSMAC/Login.Designer.cs
+++ POSMAC/Login.Designer.cs
@@ -45,7 +45,7 @@
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(153, 202);
+            this.button1.Location = new System.Drawing.Point(94, 202);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(152, 55);
             this.button1.TabIndex = 0;
@@ -60,7 +60,7 @@
             this.button2.ForeColor = System.Drawing.Color.White;
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(345, 202);
+            this.button2.Location = new System.Drawing.Point(294, 202);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(152, 55);
             this.button2.TabIndex = 1;
@@ -111,7 +111,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label3.Location = new System.Drawing.Point(159, 19);
+            this.label3.Location = new System.Drawing.Point(154, 14);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(233, 54);
             this.label3.TabIndex = 6;
@@ -121,7 +121,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.CancelButton = this.button2;
             this.ClientSize = new System.Drawing.Size(532, 319);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -130,9 +130,11 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Login";
+            this.Resizable = false;
+            this.ShadowType = MetroFramework.Forms.MetroForm.MetroFormShadowType.DropShadow;
+            this.Style = MetroFramework.MetroColorStyle.Blue;
             this.Text = "Login";
             this.ResumeLayout(false);
             this.PerformLayout();
--- POSMAC/Login.cs
+++ POSMAC/Login.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace POSMAC
 {
-    public partial class Login : Form
+    public partial class Login : MetroFramework.Forms.MetroForm
     {
         public Login()
         {
